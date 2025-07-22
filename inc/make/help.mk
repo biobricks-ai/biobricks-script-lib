e776d06f02620d734eb7585d53494087022a37d4
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-07-22 15:10:51 -0400

    feat(make): Add common GNU Make includes

---
+++ inc/make/help.mk
@@ -0,0 +1,9 @@
+# Help target for Makefiles
+# This file can be included in Makefiles to provide a standard help target
+
+# Standard help target
+help: ## Show this help message
+	@echo "Available targets:"
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-30s\033[0m %s\n", $$1, $$2}'
+
+.PHONY: help
