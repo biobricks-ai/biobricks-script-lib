780985d97077889381f69c97a549ccc2bb3a2136
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-08-13 12:40:52 -0400

    feat(bio_bricks-common): Init lib

---
+++ component/perl-bio_bricks-common/lib/Bio_Bricks/Common.pm
@@ -0,0 +1,25 @@
+package Bio_Bricks::Common;
+# ABSTRACT: Core library for BioBricks
+
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bio_Bricks::Common - Core library for BioBricks
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 EXAMPLES
+
+=head1 SEE ALSO
+
+L<https://biobricks.ai/>
+
+=cut
