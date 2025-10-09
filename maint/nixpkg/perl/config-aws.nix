eff25a37655ed9e47b9d5fb1dc5a4e9fa3cc06cc
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 13:39:27 -0400

    build(nix,perl): Add `Config-AWS`

---
+++ maint/nixpkg/perl/config-aws.nix
@@ -0,0 +1,16 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlModule {
+	pname = "Config-AWS";
+	version = "0.12";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/J/JJ/JJATRIA/Config-AWS-0.12.tar.gz";
+		hash = "sha256-FQnRvzi/cpHuouXEj83GvSWQS8h6tstv/qZANkewa1o=";
+	};
+	buildInputs = with perlPackages; [ ModuleBuildTiny ];
+	propagatedBuildInputs = with perlPackages; [ ExporterTiny PathTiny RefUtil ];
+	meta = {
+		description = "Parse AWS config files";
+		license = with lib.licenses; [ artistic1 gpl1Plus ];
+	};
+}
