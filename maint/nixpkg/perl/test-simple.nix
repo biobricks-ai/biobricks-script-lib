47cfdd91bae058b4d3531bfb5ce170ad2dd8dbff
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 13:39:28 -0400

    build(nix,perl): Add `Test-Simple`

---
+++ maint/nixpkg/perl/test-simple.nix
@@ -0,0 +1,14 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlPackage {
+	pname = "Test-Simple";
+	version = "1.302214";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/E/EX/EXODIST/Test-Simple-1.302214.tar.gz";
+		hash = "sha256-YHfsw183sRs7dd8tC6G5ylQfHcJLK+jhW26R944uA/w=";
+	};
+	meta = {
+		description = "Basic utilities for writing tests";
+		license = with lib.licenses; [ artistic1 gpl1Plus ];
+	};
+}
