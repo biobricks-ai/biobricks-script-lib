f8cda77d9615e7458649c61c422cf02b6a6f891f
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 15:54:42 -0400

    fix(nix,perl): `Test2-Tools-LoadModule`: Use correct name for `TestSimple13`

--- maint/nixpkg/perl/test2-tools-loadmodule.nix
+++ maint/nixpkg/perl/test2-tools-loadmodule.nix
@@ -7,7 +7,7 @@ perlPackages.buildPerlPackage {
 		url = "mirror://cpan/authors/id/W/WY/WYANT/Test2-Tools-LoadModule-0.008.tar.gz";
 		hash = "sha256-ktG61LY3t+PEDi9NDxhSeFXpk/J6ZZUyJDPAVpZSCvI=";
 	};
-	buildInputs = with perlPackages; [ Test2Suite ];
+	buildInputs = with perlPackages; [ TestSimple13 ];
 	meta = {
 		description = "Test whether a module can be successfully loaded";
 		license = with lib.licenses; [ artistic1 gpl1Plus ];
