fc2afa29de3926a195435da1a89b45bafde0ed95
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 15:54:42 -0400

    fix(nix,perl): `Test-Simple`: Add dep `TermTable`

--- maint/nixpkg/perl/test-simple.nix
+++ maint/nixpkg/perl/test-simple.nix
@@ -7,6 +7,7 @@ perlPackages.buildPerlPackage {
 		url = "mirror://cpan/authors/id/E/EX/EXODIST/Test-Simple-1.302214.tar.gz";
 		hash = "sha256-YHfsw183sRs7dd8tC6G5ylQfHcJLK+jhW26R944uA/w=";
 	};
+	propagatedBuildInputs = with perlPackages; [ TermTable ];
 	meta = {
 		description = "Basic utilities for writing tests";
 		license = with lib.licenses; [ artistic1 gpl1Plus ];
