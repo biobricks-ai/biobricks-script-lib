5adb7bce382f86f9d3ca62a7f0eaa53d8334f537
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 13:39:27 -0400

    build(nix,perl): Add `kura`

---
+++ maint/nixpkg/perl/kura.nix
@@ -0,0 +1,16 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlModule {
+	pname = "kura";
+	version = "0.09";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/K/KF/KFLY/kura-0.09.tar.gz";
+		hash = "sha256-bXUpRUVrKW+tvO53qiEj4GwTEuDhiGxc91fgBrs/eoE=";
+	};
+	buildInputs = with perlPackages; [ ModuleBuildTiny ];
+	meta = {
+		homepage = "https://github.com/kfly8/kura";
+		description = "Store constraints for Data::Checks, Type::Tiny, Moose, and more";
+		license = with lib.licenses; [ artistic1 gpl1Plus ];
+	};
+}
