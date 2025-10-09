054825262a801e3d4d7f4b3672469d3a3fbd58d6
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-09 13:39:28 -0400

    build(nix,perl): Add `Paws`

---
+++ maint/nixpkg/perl/paws.nix
@@ -0,0 +1,16 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlModule {
+	pname = "Paws";
+	version = "0.46";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/J/JR/JROBINSON/Paws-0.46.tar.gz";
+		hash = "sha256-lh45QSi0w48OcRfLqAOg04UBYm/580uO4A+tmIbqTb8=";
+	};
+	buildInputs = with perlPackages; [ ClassUnload FileSlurper ModuleBuildTiny PathClass TestException TestTimer TestWarnings YAML ];
+	propagatedBuildInputs = with perlPackages; [ ARGVStruct ConfigAWS DataCompare DataStructFlat DateTime DateTimeFormatISO8601 FileHomeDir IOSocketSSL JSONMaybeXS ModuleFind Moose MooseXClassAttribute MooseXGetopt NetAmazonSignatureV4 PathTiny StringCRC32 Throwable URI URITemplate URLEncode URLEncodeXS XMLSimple ];
+	meta = {
+		description = "A Perl SDK for AWS (Amazon Web Services) APIs";
+		license = lib.licenses.asl20;
+	};
+}
