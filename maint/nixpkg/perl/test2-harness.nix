0a6b0b85eecd564963434c85e558bbefca620dab
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-14 14:12:37 -0400

    build(nix,perl): Add `Test2-Harness`

---
+++ maint/nixpkg/perl/test2-harness.nix
@@ -0,0 +1,15 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlPackage {
+	pname = "Test2-Harness";
+	version = "1.000161";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/E/EX/EXODIST/Test2-Harness-1.000161.tar.gz";
+		hash = "sha256-SXO3mx7tUwVxXuc9itySNtp5XH1AkNg7FQ6hMc1ltBQ=";
+	};
+	propagatedBuildInputs = with perlPackages; [ DataUUID Importer LongJump ScopeGuard Test2PluginMemUsage Test2PluginUUID YAMLTiny gotofile ];
+	meta = {
+		description = "A new and improved test harness with better Test2 integration";
+		license = with lib.licenses; [ artistic1 gpl1Plus ];
+	};
+}
