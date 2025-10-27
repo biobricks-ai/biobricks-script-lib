12a619e0778facc16e7e611ff502a2395ae3936b
Zakariyya Mughal <zaki.mughal@gmail.com>
2025-10-27 13:04:25 -0400

    build(nix,perl): Add `Devel-Cover`

---
+++ maint/nixpkg/perl/devel-cover.nix
@@ -0,0 +1,16 @@
+{ perlPackages, fetchurl, lib }:
+
+perlPackages.buildPerlPackage {
+	pname = "Devel-Cover";
+	version = "1.51";
+	src = fetchurl {
+		url = "mirror://cpan/authors/id/P/PJ/PJCJ/Devel-Cover-1.51.tar.gz";
+		hash = "sha256-vxk2l3Anus0kPRcTd4UHFJotomnn5ynPcIsYYN7o9Yo=";
+	};
+	propagatedBuildInputs = with perlPackages; [ HTMLParser ];
+	meta = {
+		homepage = "https://pjcj.net/perl.html";
+		description = "Code coverage metrics for Perl";
+		license = with lib.licenses; [ artistic1 gpl1Plus ];
+	};
+}
